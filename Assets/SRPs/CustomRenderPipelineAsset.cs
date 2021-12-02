7ce88b49449bdac6f32a17bfb8029173c92bca4d
liuweining930905@gmail.com <liuweining930905@gmail.com>
2021-12-02 16:37:58 +0800

    统一srp脚本的缩进为空格

--- Assets/SRPs/CustomRenderPipelineAsset.cs
+++ Assets/SRPs/CustomRenderPipelineAsset.cs
@@ -7,14 +7,14 @@ using UnityEngine.Rendering;
 [CreateAssetMenu(menuName = "Rendering/Custom Render Pipeline")]
 public class CustomRenderPipelineAsset : RenderPipelineAsset
 {
-    [SerializeField] private bool dynamicBatching; // 被序列化的属性会显示到editor面板中
-    [SerializeField] private bool instancing;
+	[SerializeField] private bool dynamicBatching; // 被序列化的属性会显示到editor面板中
+	[SerializeField] private bool instancing;
 
-    // Unity 在渲染第一帧之前调用此方法。
-    // 如果渲染管线资源上的设置改变，Unity 将销毁当前的渲染管线实例，并在渲染下一帧之前再次调用此方法。
-    protected override RenderPipeline CreatePipeline()
-    {
-        // 实例化此自定义 SRP 用于渲染的渲染管线。
-        return new CustomRenderPipeline(dynamicBatching, instancing);
-    }
+	// Unity 在渲染第一帧之前调用此方法。
+	// 如果渲染管线资源上的设置改变，Unity 将销毁当前的渲染管线实例，并在渲染下一帧之前再次调用此方法。
+	protected override RenderPipeline CreatePipeline()
+	{
+		// 实例化此自定义 SRP 用于渲染的渲染管线。
+		return new CustomRenderPipeline(dynamicBatching, instancing);
+	}
 }
